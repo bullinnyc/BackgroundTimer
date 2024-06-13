af920baa8025b4aed9dad1d0abb065a28a860a44
bullinnyc <i8u11@icloud.com>
2024-06-13 13:33:26 +0700

    Initial Commit

---
+++ BackgroundTimer/BackgroundTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundTimerApp.swift
+//  BackgroundTimer
+//
+//  Created by Dmitry Kononchuk on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
